5a8e588b8820500ad3741644178166b4ef117e08
yunatoma <hautree0927@gmail.com>
2025-11-06 16:47:29 +0900

    add: ファビコンとアップルアイコンの設定

--- header.php
+++ header.php
@@ -3,7 +3,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.svg" />
+
+    <!-- ファビコン・アイコン設定 -->
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" />
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.svg" />
     <link
       rel="stylesheet"
       type="text/css"
